3bc52db42e9a9b0a2372bcdf3fc354064bb5168d
Msohaib-maker <afzalsohaib59@gmail.com>
2025-12-03 23:17:20 +0500

    deploy change

--- backend/prisma/migrations/20251130121541_add_feeds_model/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "JobFeed" ADD COLUMN     "userId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "JobFeed" ADD CONSTRAINT "JobFeed_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
