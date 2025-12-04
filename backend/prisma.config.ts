bda8d868afd19b841c4d90094a3236158a42093e
Msohaib-maker <afzalsohaib59@gmail.com>
2025-12-04 18:44:36 +0500

    prisma changes

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -14,6 +14,8 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DIRECT_URL,
+    url:
+      process.env.DIRECT_URL! ||
+      "postgresql://postgres.moppbxflxjwwfrplqaff:321%21Lo3k%23%21123%3F@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres?sslmode=require",
   },
 });
--- backend/src/modules/prisma/prisma.service.ts
+++ backend/src/modules/prisma/prisma.service.ts
@@ -10,7 +10,9 @@ export class PrismaService
 {
   constructor() {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
+      connectionString:
+        process.env.DATABASE_URL! ||
+        "postgresql://postgres.moppbxflxjwwfrplqaff:321%21Lo3k%23%21123%3F@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true",
     });
 
     super({ adapter });
